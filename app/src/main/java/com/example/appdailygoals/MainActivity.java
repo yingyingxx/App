35ad421fea1fe81ef80493fa4fd7dbb95d4f97c4
yingyingxx <yingying.xu17@gmail.com>
2020-01-19 14:15:56 +0100

    calendar, header and footer

--- app/src/main/java/com/example/appdailygoals/MainActivity.java
+++ app/src/main/java/com/example/appdailygoals/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.appdailygoals;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+            }
+        }
